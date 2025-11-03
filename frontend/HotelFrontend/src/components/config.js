bb394a5d85a3138bea399c7552c6cf36c545c550
ruchithavemula <2300032021@kluniversity.in>
2025-11-03 13:18:18 +0530

    updated

--- frontend/HotelFrontend/src/components/config.js
+++ frontend/HotelFrontend/src/components/config.js
@@ -1,2 +1,1 @@
-export const API_URL = "http://localhost:2000/api/hotels";
-//for normal backend port number  "http://localhost:2000/api/hotels": 
+export const API_URL = "http://localhost:2090/api/hotels";
